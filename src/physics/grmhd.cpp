cfb8bffe6e13b87d7a1b8caac42caa3567866616
Natey Kuebler <kuebler@users.itp.uni-frankfurt.de>
2024-07-03 20:47:12 +0200

    Added 2D KHI as initial condition

--- src/physics/grmhd.cpp
+++ src/physics/grmhd.cpp
@@ -49,11 +49,169 @@
 namespace grace{
 
 template< typename eos_t >
-static void set_grmhd_kelvinhelmholtz_initial_data() {
+static void set_grmhd_shocktube_initial_data() {
     using namespace grace ;
     using namespace Kokkos ; 
 
     GRACE_VERBOSE("Setting Shocktube initial data.") ; 
+
+    auto const rho_L = get_param<double>("grmhd","shocktube_rho_L") ; 
+    auto const rho_R = get_param<double>("grmhd","shocktube_rho_R") ; 
+    auto const press_L = get_param<double>("grmhd","shocktube_press_L") ; 
+    auto const press_R = get_param<double>("grmhd","shocktube_press_R") ;
+
+    auto& aux   = variable_list::get().getaux() ; 
+    auto& state = variable_list::get().getstate() ;
+    int64_t nx,ny,nz ; 
+    std::tie(nx,ny,nz) = amr::get_quadrant_extents() ; 
+    int ngz = amr::get_n_ghosts() ; 
+    
+    int64_t nq = amr::get_local_num_quadrants() ;
+
+    auto& coord_system = grace::coordinate_system::get() ; 
+    auto h_state_mirror = Kokkos::create_mirror_view(aux) ; 
+
+
+    int64_t ncells = EXPR((nx+2*ngz),*(ny+2*ngz),*(nz+2*ngz))*nq ;
+    for( int64_t icell=0; icell<ncells; ++icell) {
+        size_t const i = icell%(nx+2*ngz); 
+        size_t const j = (icell/(nx+2*ngz)) % (ny+2*ngz) ;
+        #ifdef GRACE_3D 
+        size_t const k = 
+            (icell/(nx+2*ngz)/(ny+2*ngz)) % (nz+2*ngz) ; 
+        size_t const q = 
+            (icell/(nx+2*ngz)/(ny+2*ngz)/(nz+2*ngz)) ;
+        #else 
+        size_t const q = (icell/(nx+2*ngz)/(ny+2*ngz)) ; 
+        #endif 
+        /* Physical coordinates of cell center */
+        auto pcoords = coord_system.get_physical_coordinates(
+            {VEC(i,j,k)},
+            q,
+            true
+        ) ; 
+        h_state_mirror(VEC(i,j,k),VELX,q) = 0. ;
+        h_state_mirror(VEC(i,j,k),VELY,q) = 0. ;
+        h_state_mirror(VEC(i,j,k),VELZ,q) = 0. ;
+        h_state_mirror(VEC(i,j,k),ZVECX,q) = 0. ;
+        h_state_mirror(VEC(i,j,k),ZVECY,q) = 0. ;
+        h_state_mirror(VEC(i,j,k),ZVECZ,q) = 0. ;
+        if ( pcoords[0] <= 0 ) {
+            h_state_mirror(VEC(i,j,k),RHO,q) = rho_L ;
+            h_state_mirror(VEC(i,j,k),PRESS,q) = press_L ;
+        } else {
+            h_state_mirror(VEC(i,j,k),RHO,q) = rho_R ;
+            h_state_mirror(VEC(i,j,k),PRESS,q) = press_R ;
+        }
+    }
+    Kokkos::deep_copy(aux,h_state_mirror) ;
+    
+    auto const& _eos = eos::get().get_eos<eos_t>() ; 
+    parallel_for( GRACE_EXECUTION_TAG("ID","shocktube_ID")
+                , MDRangePolicy<Rank<GRACE_NSPACEDIM+1>,default_execution_space>({VEC(0,0,0),0},{VEC(nx+2*ngz,ny+2*ngz,nz+2*ngz),nq})
+                , KOKKOS_LAMBDA (VEC(int const& i, int const& j, int const& k), int const& q)
+                {
+                    double h, csnd2; 
+                    double ye = _eos.ye_atmosphere();
+                    unsigned int err ; 
+                    /* Set eps temp and entropy */
+                    aux(VEC(i,j,k),EPS_,q) = 
+                        _eos.eps_h_csnd2_temp_entropy__press_rho_ye( h, csnd2, aux(VEC(i,j,k),TEMP_,q)
+                                                                   , aux(VEC(i,j,k),ENTROPY_,q)
+                                                                   , aux(VEC(i,j,k),PRESS_,q)
+                                                                   , aux(VEC(i,j,k),RHO_,q)
+                                                                   , ye,err);
+                    /* Set ye */
+                    aux(VEC(i,j,k),YE_,q) = ye ; 
+                }) ;
+}
+
+
+template< typename eos_t >
+static void set_grmhd_spherical_blastwave_initial_data() {
+    using namespace grace ;
+    using namespace Kokkos ; 
+
+    GRACE_VERBOSE("Setting spherical blastwave initial data.") ; 
+
+    auto const press_fact = get_param<double>("grmhd","blastwave_over_pressure_factor") ; 
+    auto const rblast     = get_param<double>("grmhd","blastwave_initial_radius") ; 
+
+    auto& aux   = variable_list::get().getaux() ; 
+    auto& state = variable_list::get().getstate() ;
+    int64_t nx,ny,nz ; 
+    std::tie(nx,ny,nz) = amr::get_quadrant_extents() ; 
+    int ngz = amr::get_n_ghosts() ; 
+    
+    int64_t nq = amr::get_local_num_quadrants() ;
+
+    auto& coord_system = grace::coordinate_system::get() ; 
+    auto h_state_mirror = Kokkos::create_mirror_view(aux) ; 
+
+
+    int64_t ncells = EXPR((nx+2*ngz),*(ny+2*ngz),*(nz+2*ngz))*nq ;
+    for( int64_t icell=0; icell<ncells; ++icell) {
+        size_t const i = icell%(nx+2*ngz); 
+        size_t const j = (icell/(nx+2*ngz)) % (ny+2*ngz) ;
+        #ifdef GRACE_3D 
+        size_t const k = 
+            (icell/(nx+2*ngz)/(ny+2*ngz)) % (nz+2*ngz) ; 
+        size_t const q = 
+            (icell/(nx+2*ngz)/(ny+2*ngz)/(nz+2*ngz)) ;
+        #else 
+        size_t const q = (icell/(nx+2*ngz)/(ny+2*ngz)) ; 
+        #endif 
+        /* Physical coordinates of cell center */
+        auto pcoords = coord_system.get_physical_coordinates(
+            {VEC(i,j,k)},
+            q,
+            true
+        ) ; 
+
+        double const r = 
+            sqrt( EXPR( pcoords[0]*pcoords[0], + pcoords[1]*pcoords[1], + pcoords[2] * pcoords[2])) ;
+
+        
+        h_state_mirror(VEC(i,j,k),VELX,q) = 0. ;
+        h_state_mirror(VEC(i,j,k),VELY,q) = 0. ;
+        h_state_mirror(VEC(i,j,k),VELZ,q) = 0. ;
+        h_state_mirror(VEC(i,j,k),ZVECX,q) = 0. ;
+        h_state_mirror(VEC(i,j,k),ZVECY,q) = 0. ;
+        h_state_mirror(VEC(i,j,k),ZVECZ,q) = 0. ;
+        h_state_mirror(VEC(i,j,k),RHO,q) = 1. ;
+        h_state_mirror(VEC(i,j,k),PRESS,q) = 0.1 ;
+        if ( r <= rblast ) {
+            h_state_mirror(VEC(i,j,k),PRESS,q) *= press_fact ; 
+        }
+    }
+    Kokkos::deep_copy(aux,h_state_mirror) ;
+    
+    auto const& _eos = eos::get().get_eos<eos_t>() ; 
+    parallel_for( GRACE_EXECUTION_TAG("ID","shocktube_ID")
+                , MDRangePolicy<Rank<GRACE_NSPACEDIM+1>,default_execution_space>({VEC(0,0,0),0},{VEC(nx+2*ngz,ny+2*ngz,nz+2*ngz),nq})
+                , KOKKOS_LAMBDA (VEC(int const& i, int const& j, int const& k), int const& q)
+                {
+                    double h, csnd2; 
+                    double ye = _eos.ye_atmosphere();
+                    unsigned int err ; 
+                    /* Set eps temp and entropy */
+                    aux(VEC(i,j,k),EPS_,q) = 
+                        _eos.eps_h_csnd2_temp_entropy__press_rho_ye( h, csnd2, aux(VEC(i,j,k),TEMP_,q)
+                                                                   , aux(VEC(i,j,k),ENTROPY_,q)
+                                                                   , aux(VEC(i,j,k),PRESS_,q)
+                                                                   , aux(VEC(i,j,k),RHO_,q)
+                                                                   , ye,err);
+                    /* Set ye */
+                    aux(VEC(i,j,k),YE_,q) = ye ; 
+                }) ;
+}
+
+template< typename eos_t >
+static void set_grmhd_kelvinhelmholtz_initial_data() {
+    using namespace grace ;
+    using namespace Kokkos ; 
+
+    GRACE_VERBOSE("Setting KHI initial data.") ; 
 
     /* 
        We implement here the 2D initial conditions for the KHI following
@@ -144,96 +302,15 @@ static void set_grmhd_kelvinhelmholtz_initial_data() {
                     aux(VEC(i,j,k),YE_,q) = ye ; 
                 }) ;
 }
-
-
-template< typename eos_t >
-static void set_grmhd_spherical_blastwave_initial_data() {
-    using namespace grace ;
-    using namespace Kokkos ; 
-
-    GRACE_VERBOSE("Setting Shocktube initial data.") ; 
-
-    auto const press_fact = get_param<double>("grmhd","blastwave_over_pressure_factor") ; 
-    auto const rblast     = get_param<double>("grmhd","blastwave_initial_radius") ; 
-
-    auto& aux   = variable_list::get().getaux() ; 
-    auto& state = variable_list::get().getstate() ;
-    int64_t nx,ny,nz ; 
-    std::tie(nx,ny,nz) = amr::get_quadrant_extents() ; 
-    int ngz = amr::get_n_ghosts() ; 
-    
-    int64_t nq = amr::get_local_num_quadrants() ;
-
-    auto& coord_system = grace::coordinate_system::get() ; 
-    auto h_state_mirror = Kokkos::create_mirror_view(aux) ; 
-
-
-    int64_t ncells = EXPR((nx+2*ngz),*(ny+2*ngz),*(nz+2*ngz))*nq ;
-    for( int64_t icell=0; icell<ncells; ++icell) {
-        size_t const i = icell%(nx+2*ngz); 
-        size_t const j = (icell/(nx+2*ngz)) % (ny+2*ngz) ;
-        #ifdef GRACE_3D 
-        size_t const k = 
-            (icell/(nx+2*ngz)/(ny+2*ngz)) % (nz+2*ngz) ; 
-        size_t const q = 
-            (icell/(nx+2*ngz)/(ny+2*ngz)/(nz+2*ngz)) ;
-        #else 
-        size_t const q = (icell/(nx+2*ngz)/(ny+2*ngz)) ; 
-        #endif 
-        /* Physical coordinates of cell center */
-        auto pcoords = coord_system.get_physical_coordinates(
-            {VEC(i,j,k)},
-            q,
-            true
-        ) ; 
-
-        double const r = 
-            sqrt( EXPR( pcoords[0]*pcoords[0], + pcoords[1]*pcoords[1], + pcoords[2] * pcoords[2])) ;
-
-        
-        h_state_mirror(VEC(i,j,k),VELX,q) = 0. ;
-        h_state_mirror(VEC(i,j,k),VELY,q) = 0. ;
-        h_state_mirror(VEC(i,j,k),VELZ,q) = 0. ;
-        h_state_mirror(VEC(i,j,k),ZVECX,q) = 0. ;
-        h_state_mirror(VEC(i,j,k),ZVECY,q) = 0. ;
-        h_state_mirror(VEC(i,j,k),ZVECZ,q) = 0. ;
-        h_state_mirror(VEC(i,j,k),RHO,q) = 1. ;
-        h_state_mirror(VEC(i,j,k),PRESS,q) = 0.1 ;
-        if ( r <= rblast ) {
-            h_state_mirror(VEC(i,j,k),PRESS,q) *= press_fact ; 
-        }
-    }
-    Kokkos::deep_copy(aux,h_state_mirror) ;
-    
-    auto const& _eos = eos::get().get_eos<eos_t>() ; 
-    parallel_for( GRACE_EXECUTION_TAG("ID","shocktube_ID")
-                , MDRangePolicy<Rank<GRACE_NSPACEDIM+1>,default_execution_space>({VEC(0,0,0),0},{VEC(nx+2*ngz,ny+2*ngz,nz+2*ngz),nq})
-                , KOKKOS_LAMBDA (VEC(int const& i, int const& j, int const& k), int const& q)
-                {
-                    double h, csnd2; 
-                    double ye = _eos.ye_atmosphere();
-                    unsigned int err ; 
-                    /* Set eps temp and entropy */
-                    aux(VEC(i,j,k),EPS_,q) = 
-                        _eos.eps_h_csnd2_temp_entropy__press_rho_ye( h, csnd2, aux(VEC(i,j,k),TEMP_,q)
-                                                                   , aux(VEC(i,j,k),ENTROPY_,q)
-                                                                   , aux(VEC(i,j,k),PRESS_,q)
-                                                                   , aux(VEC(i,j,k),RHO_,q)
-                                                                   , ye,err);
-                    /* Set ye */
-                    aux(VEC(i,j,k),YE_,q) = ye ; 
-                }) ;
-}
-
 template< typename eos_t >
 void set_grmhd_initial_data() {
     auto const id_type = get_param<std::string>("grmhd","id_type") ; 
-    if( id_type == "kelvinhelmholtz" ) {
-        set_grmhd_kelvinhelmholtz_initial_data<eos_t>() ; 
-    } else if( id_type == "shocktube" ) {
+    if( id_type == "shocktube" ) {
         set_grmhd_shocktube_initial_data<eos_t>() ; 
     } else if ( id_type == "blastwave" ) {
         set_grmhd_spherical_blastwave_initial_data<eos_t>() ; 
+    } else if ( id_type == "kelvinhelmholtz" ) {
+        set_grmhd_kelvinhelmholtz_initial_data<eos_t>() ; 
     } else {
         ERROR("Unrecognized id_type " << id_type ) ; 
     }
