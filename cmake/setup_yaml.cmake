e7fc3d5072b686f8629161858884a6578f405c59
Carlo Musolino <musolino@itp.uni-frankfurt.de>
2024-11-28 14:57:31 +0100

    Fix setup_yaml to correctly setup the target interface for new versions of the lib. Add missing include in connectivity.hh

--- cmake/setup_yaml.cmake
+++ cmake/setup_yaml.cmake
@@ -6,16 +6,22 @@ endif()
 
 message(STATUS "Searching path ${YAML_ROOT}")
 
-find_package( yaml-cpp REQUIRED PATHS "${YAML_ROOT}" ) 
+find_package(yaml-cpp REQUIRED PATHS "${YAML_ROOT}")
 
-message(STATUS "yaml-cpp libraries: ${YAML_CPP_LIBRARIES}")
-message(STATUS "yaml-cpp includes: ${YAML_CPP_INCLUDE_DIR}")
+if(TARGET yaml-cpp)
+    message(STATUS "Using modern yaml-cpp target")
+    add_library(yaml_cpp::yaml ALIAS yaml-cpp)
+else()
+    message(WARNING "Using legacy yaml-cpp variables")
+    if(NOT DEFINED YAML_CPP_LIBRARIES OR NOT DEFINED YAML_CPP_INCLUDE_DIRS)
+        message(FATAL_ERROR "Legacy yaml-cpp variables are not defined. Please check your installation.")
+    endif()
 
-if( NOT TARGET yaml_cpp::yaml )
-    add_library( yaml_cpp::yaml IMPORTED INTERFACE )
-
-    set_property(TARGET yaml_cpp::yaml APPEND PROPERTY 
-                 INTERFACE_INCLUDE_DIRECTORIES  "${YAML_CPP_INCLUDE_DIRS}")
-    set_property(TARGET yaml_cpp::yaml APPEND PROPERTY 
-                 INTERFACE_LINK_LIBRARIES  "${YAML_CPP_LIBRARIES}")
+    if(NOT TARGET yaml_cpp::yaml)
+        add_library(yaml_cpp::yaml IMPORTED INTERFACE)
+        set_property(TARGET yaml_cpp::yaml APPEND PROPERTY
+                     INTERFACE_INCLUDE_DIRECTORIES "${YAML_CPP_INCLUDE_DIRS}")
+        set_property(TARGET yaml_cpp::yaml APPEND PROPERTY
+                     INTERFACE_LINK_LIBRARIES "${YAML_CPP_LIBRARIES}")
+    endif()
 endif()
