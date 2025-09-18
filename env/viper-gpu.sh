ee66d1aac8c9dd4c3a887118d1948583b2371481
Carlo Musolino <carlo.musolino@aei.mpg.de>
2025-09-18 15:06:03 +0200

    Add viper env file

---
+++ env/viper-gpu.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+module load gcc/14 rocm/6.3 openmpi/5.0 hdf5-mpi
+
+export LIB_BASEDIR=/u/cmusolino/grace-libs
+
+export KOKKOS_TOOLS_ROOT=${LIB_BASEDIR}/install/kokkos-tools
+export KOKKOS_ROOT=${LIB_BASEDIR}/install/kokkos
+export P4EST_ROOT=${LIB_BASEDIR}/install/p4est
+export YAML_ROOT=${LIB_BASEDIR}/install/yamlcpp
+export CATCH2_ROOT=${LIB_BASEDIR}/install/catch2
+export SPDLOG_ROOT=${LIB_BASEDIR}/install/spdlog
+
+export KOKKOS_TOOLS_PATH=${KOKKOS_TOOLS_ROOT}/lib64
+
+export LD_LIBRARY_PATH=${KOKKOS_TOOLS_LIB}:${LD_LIBRARY_PATH}
+export PATH=${PATH}:${KOKKOS_TOOLS_LIB}/../bin
