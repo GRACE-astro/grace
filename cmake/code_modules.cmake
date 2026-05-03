c1f7aed32ca4f1184cf81e2d41d58a49b3759053
xfpmiler <xfpmiler@hunter-login03.hsn.hunter.hww.hlrs.de>
2026-05-03 14:56:38 +0200

    Trying to merge M1 to main

--- cmake/code_modules.cmake
+++ cmake/code_modules.cmake
@@ -1,7 +1,25 @@
 option(GRACE_ENABLE_BURGERS  "Enable Burgers equation module" OFF) 
 option(GRACE_ENABLE_SCALAR_ADV  "Enable scalar advection equation module" OFF) 
 option(GRACE_ENABLE_GRMHD   "Enable GRMHD equation module"  ON)
-option(GRACE_ENABLE_M1 "Enable M1 rad transport" OFF)
+
+# M1
+option(GRACE_ENABLE_M1 "Enable M1 neutrino transport" OFF)
+option(M1_NU_THREESPECIES "Enable 3-species neutrino M1 scheme" OFF)
+option(M1_NU_FIVESPECIES "Enable 5-species neutrino M1 scheme" OFF)
+
+# --- enforce hierarchy ---
+if(M1_NU_THREESPECIES)
+    set(GRACE_ENABLE_M1 ON)
+    message(STATUS "M1 with 3-Species enabled.")
+endif()
+
+if(M1_NU_FIVESPECIES)
+    set(M1_NU_THREESPECIES ON)
+    set(GRACE_ENABLE_M1 ON)
+    message(STATUS "M1 with 5-Species enabled.")
+endif()
+# M1
+
 option(GRACE_FREEZE_HYDRO "Freeze hydrodynamics evolution" OFF)
 
 if( GRACE_ENABLE_SCALAR_ADV )
@@ -21,4 +39,3 @@ endif()
 if( GRACE_ENABLE_FUKA )
     message(STATUS "FUKA module enabled.")
 endif()
-
